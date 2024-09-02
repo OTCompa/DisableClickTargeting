68dc2e57409656e77cbcdc2fb60b333304d09662
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-09-02 01:01:32 -0500

    improve logic slightly

--- DisableClickTargeting/DisableClickTargeting.cs
+++ DisableClickTargeting/DisableClickTargeting.cs
@@ -3,30 +3,28 @@ using Dalamud.Utility.Signatures;
 using System;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
-using Dalamud.Game.ClientState.Objects.Enums;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using Dalamud.Game.ClientState.Conditions;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
 namespace SamplePlugin
 {
     internal unsafe class DisableClickTargeting : IDisposable
     {
         private const int LMB = 11;
-        private const int RMB = 106;
+        private const int RMB = 4;
 
         private Configuration Configuration;
+        private TargetSystem* targetSystem;
 
-        private delegate char GetInputStatusDelegate(long a1, long a2, long a3);
-        [Signature("E8 ?? ?? ?? ?? 4C 8B BC 24 ?? ?? ?? ?? 4C 8B B4 24 ?? ?? ?? ?? 48 8B B4 24 ?? ?? ?? ?? 48 8B 9C 24 ?? ?? ?? ??", DetourName = nameof(GetInputStatusDetour))]
+        private delegate char GetInputStatusDelegate(long a1, int a2);
+        [Signature("E8 ?? ?? ?? ?? 84 C0 74 6E 48 8B 87 ?? ?? ?? ?? 48 8B F3 48 85 C0 41 0F 95 C6", DetourName = nameof(GetInputStatusDetour))]
         private readonly Hook<GetInputStatusDelegate> _getInputStatusHook;
-
-
-        private delegate char IsInputIDClickedDelegate(UIInputData* a1, int key);
-        [Signature("E8 ?? ?? ?? ?? 84 C0 44 8B C3")]
-        private readonly IsInputIDClickedDelegate? _isInputIDClicked = null;
 
         public DisableClickTargeting(Plugin plugin)
         {
             Configuration = plugin.Configuration;
             Services.gameInteropProvider.InitializeFromAttributes(this);
+            targetSystem = TargetSystem.Instance();
             this._getInputStatusHook?.Enable();
             //this._isInputIDClickedHook?.Enable();
         }
@@ -37,58 +35,45 @@ namespace SamplePlugin
             //this._isInputIDClickedHook?.Dispose();
         }
 
-        private char GetInputStatusDetour(long a1, long a2, long a3)
+        private char GetInputStatusDetour(long a1, int a2)
         {
-            var MoTargetKind = Services.TargetManager.MouseOverTarget?.ObjectKind;
-            if (
-                Configuration.EnablePlugin &&
-                (Configuration.DisableLMB && IsInputIDClicked(LMB)) &&
-                MoTargetKind == ObjectKind.Player &&
-                Services.Condition[ConditionFlag.InCombat]
-                )
-            {
-                return (char)0;
-            }
-
-            if (
-                Configuration.EnablePlugin &&
-                (Configuration.DisableRMB && IsInputIDClicked(RMB)) &&
-                MoTargetKind == ObjectKind.Player &&
-                Services.Condition[ConditionFlag.InCombat]
-                )
-            {
-                return (char)0;
-            }
-
-            return this._getInputStatusHook.Original(a1, a2, a3);
-            
-        }
-
-
-        public unsafe bool IsInputIDClicked(int key)
-        {
-            if (this._isInputIDClicked == null)
-            {
-                throw new InvalidOperationException("IsInputIDClicked signature wasn't found!");
-            }
-            var UIInputData = Framework.Instance()->UIModule->GetUIInputData();
-            if (this._isInputIDClicked!(UIInputData, key) == 1)
-                return true;
-            return false;
-        }
-
-        /*
-        [Signature("E8 ?? ?? ?? ?? 84 C0 44 8B C3", DetourName = nameof(TestDetour))]
-        private readonly Hook<IsInputIDClickedDelegate> _isInputIDClickedHook;
-        private char TestDetour(UIInputData* a1, int a2)
-        {
-            var ret = this._isInputIDClickedHook.Original(a1, a2);
-            if (ret != 0)
+            /*
+            if (a2 == LMB)
             {
                 Services.PluginLog.Debug(a2.ToString());
             }
-            return ret;
+            */
+
+            var MoTarget = targetSystem->MouseOverTarget;
+            var MoNameplateTarget = targetSystem->MouseOverNameplateTarget;
+            if (
+                    Configuration.EnablePlugin &&
+                    (Configuration.DisableLMB && a2 == LMB) &&
+                    Services.Condition[ConditionFlag.InCombat] &&
+                    (
+                        (MoTarget != null && MoTarget->ObjectKind == ObjectKind.Pc) || 
+                        (MoNameplateTarget != null && MoNameplateTarget->ObjectKind == ObjectKind.Pc)
+                    )
+                )
+            {
+                return (char)0;
+            }
+
+            if (
+                    Configuration.EnablePlugin &&
+                    (Configuration.DisableRMB && a2 == RMB) &&
+                    Services.Condition[ConditionFlag.InCombat] &&
+                    (
+                        (MoTarget != null && MoTarget->ObjectKind == ObjectKind.Pc) ||
+                        (MoNameplateTarget != null && MoNameplateTarget->ObjectKind == ObjectKind.Pc)
+                    )
+                )
+            {
+                return (char)0;
+            }
+            
+            return this._getInputStatusHook.Original(a1, a2);
+            
         }
-        */
     }
 }
